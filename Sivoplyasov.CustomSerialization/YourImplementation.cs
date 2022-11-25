d12541787286be42d4b690e13d04f2420edbf2b7
sivoplyasov <sivoplasov_96@mail.ru>
2022-11-25 18:47:05 +0300

    Init commit

---
+++ Sivoplyasov.CustomSerialization/YourImplementation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using SerializerTests.Interfaces;
+using SerializerTests.Nodes;
+
+namespace SerializerTests.Implementations
+{
+    //Specify your class\file name and complete implementation.
+    public class JohnSmithSerializer : IListSerializer
+    {
+        //the constructor with no parameters is required and no other constructors can be used.
+        public JohnSmithSerializer()
+        {
+            //...
+        }
+
+        public async Task<ListNode> DeepCopy(ListNode head)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ListNode> Deserialize(Stream s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Serialize(ListNode head, Stream s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
