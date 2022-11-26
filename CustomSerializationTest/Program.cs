19f731e5b555bdffce36a2b723212585a1dbfd5f
sivoplyasov <sivoplasov_96@mail.ru>
2022-11-26 16:50:40 +0300

    tmp

---
+++ CustomSerializationTest/Program.cs
@@ -0,0 +1,63 @@
+﻿using SerializerTests.Implementations;
+using SerializerTests.Nodes;
+
+var eighth = new ListNode
+{
+    Data = "8",
+};
+
+var seventh = new ListNode
+{
+    Data = "7",
+    Next = eighth,
+    Random = eighth 
+};
+
+var sixth = new ListNode
+{
+    Data = "6",
+    Next = seventh,
+    Random = eighth 
+};
+
+var fifth = new ListNode
+{
+    Data = "5",
+    Next = sixth,
+    Random = eighth
+};
+
+var forth = new ListNode
+{
+    Data = "4",
+    Next = fifth,
+    Random = sixth
+};
+
+
+var third = new ListNode
+{
+    Data = "3",
+    Next = forth,
+    Random = forth
+};
+
+var second = new ListNode
+{
+    Data = "2",
+    Next = third,
+    Random = fifth
+};
+
+var head = new ListNode
+{
+    Data = "1",
+    Next = second,
+    Random = second
+};
+
+JohnSmithSerializer johnSmithSerializer = new JohnSmithSerializer();
+
+var copy = await johnSmithSerializer.DeepCopy(head);
+
+Console.ReadLine();
--- Sivoplyasov.CustomSerialization/YourImplementation.cs
+++ Sivoplyasov.CustomSerialization/YourImplementation.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Text;
+using System.Text.Json;
 using SerializerTests.Interfaces;
 using SerializerTests.Nodes;
 
@@ -17,7 +16,11 @@ namespace SerializerTests.Implementations
 
         public async Task<ListNode> DeepCopy(ListNode head)
         {
-            throw new NotImplementedException();
+            var copedNodes = new Dictionary<int, ListNode>();
+
+            var result = await MakeCopy(null, head, copedNodes);
+
+            return result;
         }
 
         public async Task<ListNode> Deserialize(Stream s)
@@ -28,6 +31,78 @@ namespace SerializerTests.Implementations
         public async Task Serialize(ListNode head, Stream s)
         {
             throw new NotImplementedException();
+        }
+
+        #region Private methods
+
+        private async Task<ListNode> MakeCopy(ListNode? prvious, ListNode nodeToCopy, Dictionary<int, ListNode> copedNodes)
+        {
+            var result = new ListNode { Data = nodeToCopy.Data, Previous = prvious };
+            copedNodes.Add(nodeToCopy.GetHashCode(), result);
+
+            if (nodeToCopy.Next != null)
+                result.Next = await MakeCopy(result, nodeToCopy.Next, copedNodes);
+
+            var resultHash = result.GetHashCode();
+            copedNodes.Add(resultHash, result);
+
+            if (nodeToCopy.Random != null)
+            {
+                if (nodeToCopy.Random == nodeToCopy)
+                {
+                    result.Random = result;
+                }
+                else
+                {
+                    var randomHash = nodeToCopy.Random.GetHashCode();
+                    result.Random = copedNodes[randomHash];
+                }
+            }
+
+            return result;
+        }
+
+        private async Task SerializeInternal(ListNode node, SerializableNode previousNode, Stream s)
+        {
+            var nodeToSerialize = new SerializableNode { HashCode = node.GetHashCode(), Data = node.Data };
+
+            if (node.Random != null)
+                nodeToSerialize.RandomHashCode = node.Random.GetHashCode();
+
+            if (previousNode != null)
+            {
+                nodeToSerialize.Previous = previousNode;
+                nodeToSerialize.PrevHashCode = previousNode.HashCode;
+            }
+
+            if (node.Next != null)
+            {
+                nodeToSerialize.NextHashCode = node.Next.GetHashCode();
+                await SerializeInternal(node.Next, nodeToSerialize, s);
+            }
+
+            await JsonSerializer.SerializeAsync(s, nodeToSerialize, typeof(SerializableNode));
+        }
+
+        #endregion
+
+        private class SerializableNode
+        {
+            public int HashCode { get; set; }
+
+            public SerializableNode? Previous { get; set; }
+
+            public SerializableNode? Next { get; set; }
+
+            public string? Data { get; set; }
+
+            public int? PrevHashCode { get; set; }
+
+            public int? NextHashCode { get; set; }
+
+            public int? RandomHashCode { get; set; }
+
+            public string? NextJsonContent { get; set; }
         }
     }
 }
